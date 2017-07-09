8bdb1c39db07d3a314f9d567a0d9012cfeb18ea2
d9magai <kumagai@sencorp.co.jp>
2017-07-09 10:13:34 +0900

    modify init.sh

--- init.sh
+++ init.sh
@@ -19,9 +19,13 @@ chmod 700 ~/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQqA1PIm7jvnxmFsq2rN/3BB6tLki7xu8cTkII98GJTkelG99aoae7gL9443S1PVT8eMa/r03Q82Tk4ZkvFXkn5HweOaQTfqiWcGcbFu4DOXKNQmuLWfOstDv6rZz1kIEypBBQupqjkE8Xs3jyDghiTNYgG5doD9PtMRX5beaitzUfFVUB1md23b0EBtz7C3gR+BR1mGdRudtFHBWG4uCfam9vRJk77cCsLBamz4RMf5X5jJdhTkyvLxScAtYvfwmhhDL3vJj6Sf30sGSOTFt1gGTWxeClNkwwaYTsFFYcsoM8o4OU9ZpIFAI+lkdlAtGqbTcXZLtaXF3rm68jT7hH' >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
-composer config -g repositories.packagist composer https://packagist.jp
-composer global require hirak/prestissimo
+if type "composer" > /dev/null 2>&1; then
+    composer self-update
+else
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+    sudo chmod +x /usr/local/bin/composer
+    composer config -g repositories.packagist composer https://packagist.jp
+    composer global require hirak/prestissimo
+fi
 
